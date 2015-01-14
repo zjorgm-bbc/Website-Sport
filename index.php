39f1c013186ec8ae624c3c49dbfc950ed8397b47
zjorgm <zjorgm@ws-zjorgm.bbcnet-zh.local>
2015-01-14 13:56:51 +0100

    Es funzt Login und so

---
+++ index.php
@@ -0,0 +1,219 @@
+
+<html>
+<head>
+<title>Hi</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+
+	<div id="site">
+		<div id="header"></div>
+		<div id="navigation">
+			<div id="social">
+				<a href='#'><svg id="twitter" class="custom-icon" version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100">
+	<path class="inner-shape" style="opacity: 1; fill: rgb(46, 167, 254);"
+							transform="translate(25,25) scale(0.5)"
+							d="M99.001,19.428c-3.606,1.608-7.48,2.695-11.547,3.184c4.15-2.503,7.338-6.466,8.841-11.189 c-3.885,2.318-8.187,4-12.768,4.908c-3.667-3.931-8.893-6.387-14.676-6.387c-11.104,0-20.107,9.054-20.107,20.223 c0,1.585,0.177,3.128,0.52,4.609c-16.71-0.845-31.525-8.895-41.442-21.131C6.092,16.633,5.1,20.107,5.1,23.813 c0,7.017,3.55,13.208,8.945,16.834c-3.296-0.104-6.397-1.014-9.106-2.529c-0.002,0.085-0.002,0.17-0.002,0.255 c0,9.799,6.931,17.972,16.129,19.831c-1.688,0.463-3.463,0.71-5.297,0.71c-1.296,0-2.555-0.127-3.783-0.363 c2.559,8.034,9.984,13.882,18.782,14.045c-6.881,5.424-15.551,8.657-24.971,8.657c-1.623,0-3.223-0.096-4.796-0.282 c8.898,5.738,19.467,9.087,30.82,9.087c36.982,0,57.206-30.817,57.206-57.543c0-0.877-0.02-1.748-0.059-2.617 C92.896,27.045,96.305,23.482,99.001,19.428z"></path>
+</svg></a> <a href="www.instagram.com"><svg id="instagram"
+						class="custom-icon" version="1.1"
+						xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100">
+	<path class="inner-shape" style="opacity: 1; fill: rgb(46, 167, 254);"
+							transform="translate(25,25) scale(0.5)"
+							d="M88.2,1H11.8C5.85,1,1.026,5.827,1.026,11.781V88.22C1.026,94.174,5.85,99,11.8,99H88.2c5.95,0,10.774-4.826,10.774-10.78 V11.781C98.973,5.827,94.149,1,88.2,1z M49.946,31.184c10.356,0,18.752,8.4,18.752,18.762c0,10.361-8.396,18.761-18.752,18.761 s-18.752-8.4-18.752-18.761S39.589,31.184,49.946,31.184z M87.513,83.615c0,2.165-1.753,3.919-3.917,3.919H16.341 c-2.164,0-3.917-1.755-3.917-3.919v-41.06h8.508c-0.589,2.35-0.904,4.807-0.904,7.34c0,16.612,13.459,30.079,30.063,30.079 s30.063-13.466,30.063-30.079c0-2.533-0.315-4.99-0.904-7.34h8.263L87.513,83.615L87.513,83.615z M87.764,27.124 c0,2.165-1.754,3.919-3.918,3.919H72.723c-2.164,0-3.917-1.755-3.917-3.919v-11.13c0-2.165,1.754-3.919,3.917-3.919h11.123 c2.165,0,3.918,1.755,3.918,3.919V27.124z"></path>
+</svg></a> <a href="#"><svg id="facebook-square" class="custom-icon"
+						version="1.1" xmlns="http://www.w3.org/2000/svg"
+						xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100">
+	<path class="inner-shape" style="opacity: 1; fill: rgb(46, 167, 254);"
+							transform="translate(25,25) scale(0.5)"
+							d="M82.667,1H17.335C8.351,1,1,8.351,1,17.336v65.329c0,8.99,7.351,16.335,16.334,16.335h65.332 C91.652,99.001,99,91.655,99,82.665V17.337C99,8.353,91.652,1.001,82.667,1L82.667,1z M84.318,50H68.375v42.875H50V50h-8.855V35.973 H50v-9.11c0-12.378,5.339-19.739,19.894-19.739h16.772V22.3H72.967c-4.066-0.007-4.57,2.12-4.57,6.078l-0.023,7.594H86.75 l-2.431,14.027V50z"></path>
+</svg></a>
+
+			</div>
+			<div id='cssmenu'>
+				<ul>
+					<li><a href='#'>Home</a></li>
+					<li class='active'><a href='#'>Football</a>
+						<ul>
+							<li><a href='#'>Premiere League</a></li>
+							<ul>
+								<li><a href='#'>Resultate</a></li>
+								<li><a href='#'>Rangliste</a></li>
+							</ul></li>
+					<li><a href='#'>Champions League</a></li>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul>
+					</li>
+					<li><a href='#'>Liga BBVA</a></li>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul>
+					</li>
+					<li><a href='#'>Europa Meisterschaft</a></li>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul>
+					</li>
+				</ul>
+				</li>
+				<li><a href='#'>Unihockey</a>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul></li>
+				<li><a href='#'>Basketball</a>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul></li>
+				<li><a href='#'>Turnen</a>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul></li>
+				<li><a href='#'>Leichtathletik</a>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul></li>
+				<li><a href='#'>Kampfsport</a>
+					<ul>
+						<li><a href='#'>Resultate</a></li>
+						<li><a href='#'>Rangliste</a></li>
+					</ul></li>
+				</ul>
+			</div>
+			<div id="login">
+				<p class="Anmelde_email">
+					<input type="email" size="45" name="email" id="name"
+						placeholder="E-mail Adresse" />
+				</p>
+				<p class="passwort_3">
+					<input type="password" name="passwort" id="name"
+						placeholder="*********" />
+				</p>
+				<p class="submit_1">
+					<input type="submit" value="Login" />
+				</p>
+				<div id="registrieren">
+					<a href='Formulare.php'>Noch kein Konto? Jetzt Registrieren!</a>
+				</div>
+			</div>
+		</div>
+		<div id="news">
+			<div id="LatestNews">Latest News</div>
+			<ul>
+				<li><div class="Rahmen">
+						<div class="datum">5.1.2015</div>
+						<div class="titel">
+							<a href='#'>Aufbau der Webseite beginnt</a>
+						</div>
+					</div></li>
+				<li><div class="Rahmen">
+						<div class="datum">23.2.2015</div>
+						<div class="titel">
+							<a href='#'>Webseite fertiggestellt</a>
+						</div>
+					</div></li>
+				<li><div class="Rahmen">
+						<div class="datum">11.2.2015</div>
+						<div class="titel">
+							<a href='#'>NBA:Luzern wird Meister</a>
+						</div>
+					</div></li>
+				<li><div class="Rahmen">
+						<div class="datum">15.2.2015</div>
+						<div class="titel">
+							<a href='#'>NLA:ZSC gewinnt 2:1</a>
+						</div>
+					</div></li>
+				<li><div class="Rahmen">
+						<div class="datum">11.3.2015</div>
+						<div class="titel">
+							<a href='#'>Simon Amman st&uumlrtzt</a>
+						</div>
+					</div></li>
+				<li><div class="Rahmen">
+						<div class="datum">31.3.2015</div>
+						<div class="titel">
+							<a href='#'>Shaqiri wechselt zu Inter</a>
+						</div>
+					</div></li>
+			</ul>
+		</div>
+		<div id="content">
+			<div id="Uberschrift">Top Stories</div>
+			<div>
+				<br> <br> <br>
+				<ul>
+					<li><div class="text">Lorem ipsum dolor sit amet, consetetur
+							sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+							labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+							eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+							gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+							amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+							sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+							aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+							duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+							takimata sanctus est Lorem ipsum dolor sit amet.</div></li>
+					<li><div class="text">Lorem ipsum dolor sit amet, consetetur
+							sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+							labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+							eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+							gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+							amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+							sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+							aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+							duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+							takimata sanctus est Lorem ipsum dolor sit amet.</div></li>
+					<li><div class="text">Lorem ipsum dolor sit amet, consetetur
+							sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+							labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+							eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+							gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+							amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+							sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+							aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+							duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+							takimata sanctus est Lorem ipsum dolor sit amet.</div></li>
+					<li><div class="text">Lorem ipsum dolor sit amet, consetetur
+							sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut
+							labore et dolore magna aliquyam erat, sed diam voluptua. At vero
+							eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+							gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+							amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+							sed diam nonumy eirmod tempor invidunt ut labore et dolore magna
+							aliquyam erat, sed diam voluptua. At vero eos et accusam et justo
+							duo dolores et ea rebum. Stet clita kasd gubergren, no sea
+							takimata sanctus est Lorem ipsum dolor sit amet.</div></li>
+				</ul>
+			</div>
+		</div>
+		<div id="footer">&copy by Miguel Jorge & Oliver Aschwanden</div>
+	</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
