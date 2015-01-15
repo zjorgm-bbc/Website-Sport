19221a38aca65ae03cb96bf92533c577d72bf4ae
zjorgm <zjorgm@ws-zjorgm.bbcnet-zh.local>
2015-01-15 13:56:19 +0100

    ist mir egal

--- ChampionsLeague.php
+++ ChampionsLeague.php
@@ -107,7 +107,76 @@
 			<div id="oli">
 				<br> <br> <br>
 				<table class="tg">
-				<p>Gruppe A</p>
+					<p>Gruppe A</p>
+					<tr>
+						<th class="vereine">Vereine</th>
+						<th class="tg">S</th>
+						<th class="tg">S</th>
+						<th class="tg">U</th>
+						<th class="tg">N</th>
+						<th class="tg">+</th>
+						<th class="tg">G</th>
+						<th class="tg">+/-</th>
+						<th class="tg">Pkt.</th>
+					</tr>
+
+					<tr>
+						<td class="vereine"><img class="teams" src="Atletico.png"
+							alt="Atletico"> Club Atl&eacute;tico de Madrid</td>
+						<td class="tg">6</td>
+						<td class="tg">4</td>
+						<td class="tg">1</td>
+						<td class="tg">1</td>
+						<td class="tg">14</td>
+						<td class="tg">3</td>
+						<td class="tg">11</td>
+						<td class="tg">13</td>
+					</tr>
+
+					<tr>
+						<div class="tabelle">
+						
+						
+						<td class="vereine_2"><img class="teams" src="Juventus.png"
+							alt="Juventus">Juventus</td>
+						<td class="tg_2"">6</td>
+						<td class="tg_2">3</td>
+						<td class="tg_2">1</td>
+						<td class="tg_2">2</td>
+						<td class="tg_2">7</td>
+						<td class="tg_2">4</td>
+						<td class="tg_2">3</td>
+						<td class="tg_2">10</td>
+						</div>
+					</tr>
+
+					<tr>
+						<td class="vereine"><img class="teams" src="Olympiacos.png"
+							alt="Juventus">Olympiacos FC</td>
+						<td class="tg">6</td>
+						<td class="tg">3</td>
+						<td class="tg">0</td>
+						<td class="tg">3</td>
+						<td class="tg">10</td>
+						<td class="tg">13</td>
+						<td class="tg">-3</td>
+						<td class="tg">9</td>
+					</tr>
+					<tr>
+						<td class="vereine_2"><img class="teams" src="malmoe.png"
+							alt="Malmoe">Malm&ouml; FF</td>
+						<td class="tg_2">6</td>
+						<td class="tg_2">1</td>
+						<td class="tg_2">0</td>
+						<td class="tg_2">3</td>
+						<td class="tg_2">10</td>
+						<td class="tg_2">13</td>
+						<td class="tg_2">-3</td>
+						<td class="tg_2">9</td>
+					</tr>
+				</table>
+				<table class="tg">
+					<p>Gruppe B</p>
 					<tr>
 						<th class="vereine">Vereine</th>
 						<th class="tg">S</th>
@@ -169,6 +238,7 @@
 					</tr>
 				</table>
 				<table class="tg">
+					<p>Gruppe C</p>
 					<tr>
 						<th class="vereine">Vereine</th>
 						<th class="tg">S</th>
@@ -230,6 +300,7 @@
 					</tr>
 				</table>
 				<table class="tg">
+					<p>Gruppe D</p>
 					<tr>
 						<th class="vereine">Vereine</th>
 						<th class="tg">S</th>
@@ -291,6 +362,7 @@
 					</tr>
 				</table>
 				<table class="tg">
+					<p>Gruppe E</p>
 					<tr>
 						<th class="vereine">Vereine</th>
 						<th class="tg">S</th>
@@ -352,6 +424,7 @@
 					</tr>
 				</table>
 				<table class="tg">
+					<p>Gruppe F</p>
 					<tr>
 						<th class="vereine">Vereine</th>
 						<th class="tg">S</th>
@@ -413,6 +486,8 @@
 					</tr>
 				</table>
 				<table class="tg">
+					<p>Gruppe G</p>
+
 					<tr>
 						<th class="vereine">Vereine</th>
 						<th class="tg">S</th>
@@ -474,67 +549,7 @@
 					</tr>
 				</table>
 				<table class="tg">
-					<tr>
-						<th class="vereine">Vereine</th>
-						<th class="tg">S</th>
-						<th class="tg">S</th>
-						<th class="tg">U</th>
-						<th class="tg">N</th>
-						<th class="tg">+</th>
-						<th class="tg">G</th>
-						<th class="tg">+/-</th>
-						<th class="tg">Pkt.</th>
-					</tr>
-					<tr>
-						<td class="vereine"><img class="teams" src="Atletico.png"
-							alt="Atletico"> Club Atl&eacute;tico de Madrid</td>
-						<td class="tg">6</td>
-						<td class="tg">4</td>
-						<td class="tg">1</td>
-						<td class="tg">1</td>
-						<td class="tg">14</td>
-						<td class="tg">3</td>
-						<td class="tg">11</td>
-						<td class="tg">13</td>
-					</tr>
-					<tr>
-						<td class="vereine"><img class="teams" src="Juventus.png"
-							alt="Juventus">Juventus</td>
-						<td class="tg">6</td>
-						<td class="tg">3</td>
-						<td class="tg">1</td>
-						<td class="tg">2</td>
-						<td class="tg">7</td>
-						<td class="tg">4</td>
-						<td class="tg">3</td>
-						<td class="tg">10</td>
-					</tr>
-					<tr>
-						<td class="vereine"><img class="teams" src="Olympiacos.png"
-							alt="Juventus">Olympiacos FC</td>
-						<td class="tg">6</td>
-						<td class="tg">3</td>
-						<td class="tg">0</td>
-						<td class="tg">3</td>
-						<td class="tg">10</td>
-						<td class="tg">13</td>
-						<td class="tg">-3</td>
-						<td class="tg">9</td>
-					</tr>
-					<tr>
-						<td class="vereine"><img class="teams" src="malmoe.png"
-							alt="Malmoe">Malm&ouml; FF</td>
-						<td class="tg">6</td>
-						<td class="tg">1</td>
-						<td class="tg">0</td>
-						<td class="tg">3</td>
-						<td class="tg">10</td>
-						<td class="tg">13</td>
-						<td class="tg">-3</td>
-						<td class="tg">9</td>
-					</tr>
-				</table>
-				<table class="tg">
+					<p>Gruppe H</p>
 					<tr>
 						<th class="vereine">Vereine</th>
 						<th class="tg">S</th>
